5fe09ba3ff0d6c52bd8a3d00c619fc990580334d
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-06-02 23:12:25 -0300

    Ajustes

--- Banco de Dados/ddl.sql
+++ Banco de Dados/ddl.sql
@@ -4,12 +4,9 @@ use sistemacatalogodb;
 create table tb_funcionario(
 	id_funcionario		int primary key auto_increment,
     nm_funcionario		varchar(100),
-    ds_email		varchar(100),
-    ds_senha		varchar(100)
+    ds_email			varchar(100),
+    ds_senha			varchar(100)
 );
-insert into tb_funcionario(nm_funcionario,ds_email,ds_senha)
-						values('user teste','teste@gmail.com','teste');
-select * from tb_funcionario;
 
 create table tb_produto(
 	id_produto			int primary key auto_increment,
@@ -20,6 +17,3 @@ create table tb_produto(
     ds_categoria		varchar(100),
     foreign key (id_funcionario) references tb_funcionario (id_funcionario)
 ); 
-drop table tb_produto;                       
-insert into tb_produto(id_funcionario,nm_produto,vl_preco,ds_categoria)
-						values(1,'teste',19.99,'Salgado');
--- Banco de Dados/dml.sql
+++ Banco de Dados/dml.sql
@@ -1,22 +1,25 @@
 use sistemacatalogodb;
 
+select * from tb_funcionario;
+select * from tb_produto;
+
 -- carga inicial usuário admin
-INSERT INTO tb_usuario (nm_usuario, ds_email, ds_senha)
+INSERT INTO tb_funcionario (nm_funcionario, ds_email, ds_senha)
      VALUES ('admin', 'admin@admin.com.br', '1234');
 
 
 -- efetuar login;
 select id_funcionario 		id,
        nm_funcionario		nome,
-       ds_email			email
+       ds_email				email
   from tb_funcionario
- where ds_email 		= 'admin@admin.com.br'
-   and ds_senha			= '1234';
+ where ds_email 		= 	'admin@admin.com.br'
+   and ds_senha			= 	'1234';
 
 
 -- cadastar produto
-insert into tb_produto(id_funcionario, nm_produto, vl_preco,ds_categoria)
-					   values(1,'macarronada',10.50,'salgados');
+insert into tb_produto(id_funcionario, nm_produto, vl_preco, ds_categoria)
+					   values(1,'macarronada', 10.50, 'salgados');
 
 
 -- alterar imagem
@@ -38,7 +41,7 @@ DELETE FROM tb_produto
       WHERE id_produto= 1;
 
    
--- consultar filme por categoria
+-- consultar produto por categoria
 SELECT	id_produto			id,
 		nm_produto  		nome,
         vl_preco			preco,
